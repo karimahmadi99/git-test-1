858ba537e402b3e3acc9090e7edc9d6214f389c5
karimahmadi <156588707+karimahmadi99@users.noreply.github.com>
2024-02-12 14:50:45 +0330

    Update t_app.py

--- t_app.py
+++ t_app.py
@@ -2,3 +2,11 @@ name = ['amir', 'sara', 'davood', 'tina']
 def hello(name):
     for names in name:
         print("helli: " + names)
+
+ints = 32
+add = 0
+while True:
+    add += 1
+    print(add)
+    if add == ints:
+        break
